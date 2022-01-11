f39acf8b148eb97a9b2e0b8d80bec35b77467864
Carlos Zuniga <carlos.zun@gmail.com>
2022-01-11 13:29:52 -0500

    Step 5: Turn off echoing

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,21 @@
 const std = @import("std");
 const os = std.os;
+const linux = os.linux;
 const mem = std.mem;
 
+fn enableRawMode() void {
+    var raw: linux.termios = undefined;
+    _ = linux.tcgetattr(linux.STDIN_FILENO, &raw);
+    raw.lflag &= ~linux.ECHO;
+    _ = linux.tcsetattr(linux.STDIN_FILENO, .FLUSH, &raw);
+}
+
 pub fn main() anyerror!void {
+    enableRawMode();
+
     var c: [1]u8 = undefined;
     var slice = c[0..c.len];
-    while ((try os.read(os.system.STDIN_FILENO, slice)) == 1 and slice[0] != 'q') {}
+    while ((try os.read(linux.STDIN_FILENO, slice)) == 1 and slice[0] != 'q') {}
 }
 
 test "basic test" {
