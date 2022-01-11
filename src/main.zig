183c1d9ea7177633966eb76abf9b6f20a1785086
Carlos Zuniga <carlos.zun@gmail.com>
2022-01-11 13:37:54 -0500

    Step 6: Disable raw mode at exit

--- src/main.zig
+++ src/main.zig
@@ -3,15 +3,22 @@ const os = std.os;
 const linux = os.linux;
 const mem = std.mem;
 
+var orig_termios: linux.termios = undefined;
+
 fn enableRawMode() void {
-    var raw: linux.termios = undefined;
-    _ = linux.tcgetattr(linux.STDIN_FILENO, &raw);
+    _ = linux.tcgetattr(linux.STDIN_FILENO, &orig_termios);
+    var raw = orig_termios;
     raw.lflag &= ~linux.ECHO;
     _ = linux.tcsetattr(linux.STDIN_FILENO, .FLUSH, &raw);
 }
 
+fn disableRawMode() void {
+    _ = linux.tcsetattr(linux.STDIN_FILENO, .FLUSH, &orig_termios);
+}
+
 pub fn main() anyerror!void {
     enableRawMode();
+    defer disableRawMode();
 
     var c: [1]u8 = undefined;
     var slice = c[0..c.len];
