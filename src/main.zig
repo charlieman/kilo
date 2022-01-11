4146f11691e1c58dd8042aadd5d57ba7df4f5294
Carlos Zuniga <carlos.zun@gmail.com>
2022-01-11 12:28:16 -0500

    Step 3: read keypresses (skipped Step 2)

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
+const os = std.os;
 
-pub fn main() anyerror!void {}
+pub fn main() anyerror!void {
+    var c: [1]u8 = undefined;
+    var slice = c[0..c.len];
+    while ((try os.read(os.system.STDIN_FILENO, slice)) == 1) {}
+}
 
 test "basic test" {
     try std.testing.expectEqual(10, 3 + 7);
