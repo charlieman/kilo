24d2e938937546a455eb81df908ee9e6b73d84bb
Carlos Zuniga <carlos.zun@gmail.com>
2022-01-11 12:38:46 -0500

    Step 4: Press q to quit

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,11 @@
 const std = @import("std");
 const os = std.os;
+const mem = std.mem;
 
 pub fn main() anyerror!void {
     var c: [1]u8 = undefined;
     var slice = c[0..c.len];
-    while ((try os.read(os.system.STDIN_FILENO, slice)) == 1) {}
+    while ((try os.read(os.system.STDIN_FILENO, slice)) == 1 and slice[0] != 'q') {}
 }
 
 test "basic test" {
